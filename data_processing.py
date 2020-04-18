a9c78a874d4cab3240ccf5da276c4b4a53d4f808
Francesca <fbaldini@caltech.edu>
2020-04-18 16:54:50 -0700

    ad

---
+++ data_processing.py
@@ -0,0 +1,34 @@
+# %%
+import pandas as pd
+import numpy as np
+import math
+from datetime import datetime
+
+# %%
+
+# we need:
+    # confirmed cases
+    # deaths
+    # healed/recovered
+    #number of individual
+
+# group
+    # location
+    # age
+    # gender
+    # pollution
+
+
+
+# parh to directory
+directory = 'data/us/covid'
+
+
+# %%
+ # check time format
+
+time_format = "%d%b%Y %H:%M"
+datetime.now().strftime(time_format)
+
+
+# %%
